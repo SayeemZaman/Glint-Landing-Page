c04e714aca577b91a422c84bb5cb9fe66c67abe6
sayeemzaman3 <sayeem312@gmail.com>
2025-08-30 23:53:38 +0000

    add Contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Clients from "./components/Clients";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <About />
       <Services />
       <Clients />
+      <Contact />
     </>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import Form from "./Form";
+import Address from "./Address";
+
+export default function Contact() {
+  return (
+    <section className="bg-jet p-0">
+      <div className="py-[48px] px-[32px]">
+        <h2 className="text-fresh-green">CONTACT US</h2>
+        <h3 className="text-white">
+          Reach out for a new project or just say hello
+        </h3>
+      </div>
+      <Form />
+      <Address />
+    </section>
+  );
+}
