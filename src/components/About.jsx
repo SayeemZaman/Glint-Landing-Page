4af6be7a09d526af11a8b0f70cb6d3d985c63ca6
sayeemzaman3 <sayeem312@gmail.com>
2025-09-01 11:43:46 +0000

    add responsive About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 function Info({ num, name }) {
   return (
-    <div className="py-[24px] flex flex-col gap-[16px]">
-      <p className="font-satoshiBold text-7xl text-white">{num}</p>
+    <div className="py-[24px] flex flex-col gap-[16px] md:gap-[48px] w-[356px]">
+      <p className="font-satoshiBold text-7xl md:8xl text-white">{num}</p>
       <p className="font-satoshiBold text-lg">{name}</p>
     </div>
   );
@@ -18,9 +18,11 @@ export default function About() {
   return (
     <section id="about" className="bg-fresh-green py-[80px]">
       <h2>HELLO THERE</h2>
-      <h3 className="text-white text-4xl">We Are Glint</h3>
-      <hr className="mt-[32px] mb-[40px] opacity-35" />
-      <p className="font-satoshiMed text-base">
+      <h3 className="text-white text-4xl md:text-5xl lg:text-7xl">
+        We Are Glint
+      </h3>
+      <hr className="mt-[32px] mb-[40px] mx-auto opacity-35 max-w-[512px]" />
+      <p className="font-satoshiMed text-base md:text-lg md:leading-relaxed lg:leading-loose md:px-[24px] lg:px-[48px]">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -28,7 +30,7 @@ export default function About() {
         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
       </p>
-      <div className="my-[32px]">
+      <div className="my-[32px] flex flex-wrap justify-center">
         {data.map((item, index) => (
           <Info key={index} name={item.name} num={item.num} />
         ))}
